c5fa86a267064649b5e1044d2d189df05a2de0ce
Yuzo <yuzokamoto@gmail.com>
2021-02-17 21:42:37 -0300

    Migrations

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
@@ -9,18 +9,22 @@ const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
   // TODO
+  response.json({ ok: 'ok' });
 });
 
 transactionsRouter.post('/', async (request, response) => {
   // TODO
+  response.json({ ok: 'ok' });
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
   // TODO
+  response.json({ ok: 'ok' });
 });
 
 transactionsRouter.post('/import', async (request, response) => {
   // TODO
+  response.json({ ok: 'ok' });
 });
 
 export default transactionsRouter;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,9 +1,23 @@
 // import AppError from '../errors/AppError';
+import { v4 as uuidv4 } from 'uuid';
+import { getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
     // TODO
   }
 }
